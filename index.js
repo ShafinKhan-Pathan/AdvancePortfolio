0df074142466fca5eed4d5f799e303205037f544
ShafinKhan-Pathan <kshafin575@gmail.com>
2024-09-22 00:49:33 -0500

    Almost finished

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 // Template ID - template_16hzvac
 // Service ID - service_5k2utos
 // User ID or Public ID - O1tF--IwQPNGNvMM0
+let isModalOpen = false
+let contrastOpen = false
+const scaleFactor = 1 / 20
+
+function moveBackground(event){
+    const shape = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    for (let i = 0; i < shape.length; i++){
+        const isOdd = i % 2 !==0
+        const boolInt = isOdd ? -1 : 1
+        shape[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast(){
+    if (contrastOpen){
+        contrastOpen = false
+        return document.body.classList.remove("dark-theme")
+    }
+    contrastOpen = true
+    document.body.classList += " dark-theme"
+}
+
 
 function contact(event){
     event.preventDefault();
@@ -16,4 +40,13 @@ function contact(event){
         loading.classList.remove("modal__overlay--loading")
         alert("The Email Service is currently unavailable. Please contact directly on kshafin575@gmail.com ")
     })
+}
+
+function toggleModal(){
+    if (isModalOpen){
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
+    document.body.classList += " modal--open"
 }
